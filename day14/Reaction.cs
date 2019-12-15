255fd768ffda8f64417d977b9de5e7febdb8d4bf
Vic <victor.s@live.dk>
2019-12-15 16:34:32 +0100

    Day 14.1

---
+++ day14/Reaction.cs
@@ -0,0 +1,11 @@
+namespace day14{
+    using System.Collections.Generic;
+    class Reaction{
+
+        public List<(int Amount, string Element)> Inputs{get;set;}
+        public List<(int Amount, string Element)> Outouts{get;set;}
+
+
+
+    }
+}
